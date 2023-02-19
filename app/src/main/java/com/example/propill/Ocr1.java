a91c6a268386e3dcd626376e3489d383dbaef554
yiejun <huey9362@gmail.com>
2023-02-20 00:59:17 +0900

    push database

--- app/src/main/java/com/example/propill/Ocr1.java
+++ app/src/main/java/com/example/propill/Ocr1.java
@@ -1,14 +1,38 @@
 package com.example.propill;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 import com.googlecode.tesseract.android.TessBaseAPI;
 
 
@@ -21,6 +45,177 @@ import java.io.OutputStream;
 
 
 public class Ocr1 extends AppCompatActivity {
+    static final int REQUEST_CODE = 2;
+
+    ImageView imageView;    // 갤러리에서 가져온 이미지를 보여줄 뷰
+    Uri uri;                // 갤러리에서 가져온 이미지에 대한 Uri
+    Bitmap bitmap;          // 갤러리에서 가져온 이미지를 담을 비트맵
+    InputImage image;       // ML 모델이 인식할 인풋 이미지
+    TextView text_info;     // ML 모델이 인식한 텍스트를 보여줄 뷰
+    Button btn_get_image, btn_detection_image;  // 이미지 가져오기 버튼, 이미지 인식 버튼
+    TextRecognizer recognizer;    //텍스트 인식에 사용될 모델
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ocr1);
+
+        imageView = findViewById(R.id.imageView);
+        text_info = findViewById(R.id.text_info);
+        recognizer = TextRecognition.getClient(new TextRecognizerOptions.Builder().build());    //텍스트 인식에 사용될 모델
+
+        // GET IMAGE 버튼
+        btn_get_image = findViewById(R.id.btn_get_image);
+        btn_get_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
+        // IMAGE DETECTION 버튼
+        btn_detection_image = findViewById(R.id.btn_detection_image);
+        btn_detection_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextRecognition(recognizer);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected  void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == REQUEST_CODE) {
+            // 갤러리에서 선택한 사진에 대한 uri를 가져온다.
+            uri = data.getData();
+
+            setImage(uri);
+        }
+    }
+
+    // uri를 비트맵으로 변환시킨후 이미지뷰에 띄워주고 InputImage를 생성하는 메서드
+    private void setImage(Uri uri) {
+        try{
+            InputStream in = getContentResolver().openInputStream(uri);
+            bitmap = BitmapFactory.decodeStream(in);
+            imageView.setImageBitmap(bitmap);
+
+            image = InputImage.fromBitmap(bitmap, 0);
+            Log.e("setImage", "이미지 to 비트맵");
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void TextRecognition(TextRecognizer recognizer){
+        Task<Text> result = recognizer.process(image)
+                // 이미지 인식에 성공하면 실행되는 리스너
+                .addOnSuccessListener(new OnSuccessListener<Text>() {
+                    @Override
+                    public void onSuccess(Text visionText) {
+                        Log.e("텍스트 인식", "성공");
+                        // Task completed successfully
+                        String resultText = visionText.getText();
+                        text_info.setText(resultText);  // 인식한 텍스트를 TextView에 세팅
+                    }
+                })
+                // 이미지 인식에 실패하면 실행되는 리스너
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e("텍스트 인식", "실패: " + e.getMessage());
+                            }
+                        });
+    }
+}
+
+/*    private SurfaceView surfaceView;
+    private TextView textView;
+    private CameraSource cameraSource;
+    private static final int PERMISSION = 100;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_ocr1);
+
+        surfaceView = findViewById(R.id.camera);
+        textView = findViewById(R.id.text1);
+
+        startCameraSource();
+
+    }
+
+    private void startCameraSource() {
+        final TextRecognizer textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+
+        if(!textRecognizer.isOperational()) {
+            Log.w("Tag", "Dependencies not loaded yet");
+        }else{
+            cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK).setRequestedPreviewSize(1280,1024)
+                    .setAutoFocusEnabled(true).setRequestedFps(2.0f).build();
+            surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(@NonNull SurfaceHolder holder) {
+                    try {
+                        if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                            ActivityCompat.requestPermissions(Ocr1.this, new String[]{Manifest.permission.CAMERA},
+                                    PERMISSION);
+                            return;
+                        }
+                        cameraSource.start(surfaceView.getHolder());
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void surfaceChanged(@NonNull SurfaceHolder holder, int i, int i1, int i2) {
+                    //Release source for cameraSource
+
+
+                }
+
+                @Override
+                public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+                    cameraSource.stop();
+
+                }
+            });
+            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+                @Override
+                public void release() {
+                    //detect all text from camera
+                }
+
+                @Override
+                public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                    final SparseArray<TextBlock> items = detections.getDetectedItems();
+                    if(items.size() != 0){
+                        textView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                StringBuilder stringBuilder = new StringBuilder();
+                                for(int i=0; i<items.size();i++){
+                                    TextBlock item = items.valueAt(i);
+                                    stringBuilder.append(item.getValue());
+                                    stringBuilder.append("\n");
+                                }
+                                textView.setText(stringBuilder.toString());
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    }*/
 /*
     Bitmap image; //사용되는 이미지
     private TessBaseAPI mTess; //Tess API reference
@@ -104,6 +299,3 @@ public class Ocr1 extends AppCompatActivity {
             }
         }
     }*/
-
-
-}
